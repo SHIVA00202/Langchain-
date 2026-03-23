0b2b72c5afd5eb3371f929cfa52e6089876af251
SHIVA00202 <shivayadav1238800@gmail.com>
2026-03-23 23:46:08 +0530

    simple chain

---
+++ simplechain.py
@@ -0,0 +1,31 @@
+from langchain_openai import ChatOpenAI
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+prompt = PromptTemplate(
+    template='Generate 5 interesting facts about {topic}',
+    input_variables=['topic']
+)
+
+llm = HuggingFaceEndpoint(
+    repo_id="openai/gpt-oss-120b",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+parser = StrOutputParser()
+
+chain = prompt | model | parser
+
+result = chain.invoke({'topic':'cricket'})
+
+print(result)
+
+
+
+
